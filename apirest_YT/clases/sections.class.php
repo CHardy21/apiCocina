ecb61a2ca3262d29beb19a2a85c5c6c2497583a5
CHardy <christianrh2000@hotmail.com>
2022-09-26 12:13:04 -0300

    work in sections db

---
+++ apirest_YT/clases/sections.class.php
@@ -0,0 +1,249 @@
+<?php
+require_once "conexion/conexion.php";
+require_once "respuestas.class.php";
+
+
+class sections extends conexion {
+
+    private $table = "sections";
+    private $section_id = "";
+    private $section_name = "";
+    private $section_active = "";
+    private $section_nro_prod = "";
+    private $token = "";
+    
+
+    public function getSectionsList($sectionStatus = 'all', $pagina = 1, $cantidad = 10){
+        $inicio  = 0 ;
+        $filtro = ""; // " WHERE sections = '$categoria' "
+        $limit = "";  // " limit $inicio,$cantidad"
+
+        // paginacion de resultados
+        if ($pagina > 1){ $inicio = ($cantidad * ($pagina - 1)) ;}
+        $limit = " limit $inicio,$cantidad";
+
+        if ($sectionStatus != 'all' ){ 
+            $filtro = " WHERE section_active = '$sectionStatus' ";
+            if($cantidad == "all") { $limit ="";}
+        }else{
+            $limit="";
+        };
+
+        // 
+        $query ="SELECT 
+                    section_id, 
+                    section_name,
+                    section_active,
+                    section_nro_prod
+                FROM " . $this->table . $filtro . $limit;
+
+        //print_r($query);
+        $query2 = "SELECT * FROM " . $this->table . $filtro ;
+        //print_r($query2);
+        $datos = parent::getDatos($query);
+        $totalResults = parent::nonQuery($query2);
+        $datos = parent::formatData("ok", $totalResults,$datos);
+        return ($datos);
+    }
+    
+    public function getProduct($id){
+        $query = "SELECT * FROM " . $this->table . " WHERE prod_id = '$id'";
+        $datos = parent::getDatos($query);
+        return ($datos);
+
+    }
+
+    public function post($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json,true);
+
+        if(!isset($datos['token'])){
+                return $_respuestas->error_401();
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken =   $this->buscarToken();
+            if($arrayToken){
+
+                if(!isset($datos['nombre']) || !isset($datos['dni']) || !isset($datos['correo'])){
+                    return $_respuestas->error_400();
+                }else{
+                    $this->nombre = $datos['nombre'];
+                    $this->dni = $datos['dni'];
+                    $this->correo = $datos['correo'];
+                    if(isset($datos['telefono'])) { $this->telefono = $datos['telefono']; }
+                    if(isset($datos['direccion'])) { $this->direccion = $datos['direccion']; }
+                    if(isset($datos['codigoPostal'])) { $this->codigoPostal = $datos['codigoPostal']; }
+                    if(isset($datos['genero'])) { $this->genero = $datos['genero']; }
+                    if(isset($datos['fechaNacimiento'])) { $this->fechaNacimiento = $datos['fechaNacimiento']; }
+                    $resp = $this->insertarPaciente();
+                    if($resp){
+                        $respuesta = $_respuestas->response;
+                        $respuesta["result"] = array(
+                            "pacienteId" => $resp
+                        );
+                        return $respuesta;
+                    }else{
+                        return $_respuestas->error_500();
+                    }
+                }
+
+            }else{
+                return $_respuestas->error_401("El Token que envio es invalido o ha caducado");
+            }
+        }
+
+
+       
+
+    }
+
+
+    private function insertarPaciente(){
+        $query = "INSERT INTO " . $this->table . " (DNI,Nombre,Direccion,CodigoPostal,Telefono,Genero,FechaNacimiento,Correo)
+        values
+        ('" . $this->dni . "','" . $this->nombre . "','" . $this->direccion ."','" . $this->codigoPostal . "','"  . $this->telefono . "','" . $this->genero . "','" . $this->fechaNacimiento . "','" . $this->correo . "')"; 
+        $resp = parent::nonQueryId($query);
+        if($resp){
+             return $resp;
+        }else{
+            return 0;
+        }
+    }
+    
+    public function put($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json,true);
+
+        if(!isset($datos['token'])){
+            return $_respuestas->error_401();
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken =   $this->buscarToken();
+            if($arrayToken){
+                if(!isset($datos['pacienteId'])){
+                    return $_respuestas->error_400();
+                }else{
+                    $this->pacienteid = $datos['pacienteId'];
+                    if(isset($datos['nombre'])) { $this->nombre = $datos['nombre']; }
+                    if(isset($datos['dni'])) { $this->dni = $datos['dni']; }
+                    if(isset($datos['correo'])) { $this->correo = $datos['correo']; }
+                    if(isset($datos['telefono'])) { $this->telefono = $datos['telefono']; }
+                    if(isset($datos['direccion'])) { $this->direccion = $datos['direccion']; }
+                    if(isset($datos['codigoPostal'])) { $this->codigoPostal = $datos['codigoPostal']; }
+                    if(isset($datos['genero'])) { $this->genero = $datos['genero']; }
+                    if(isset($datos['fechaNacimiento'])) { $this->fechaNacimiento = $datos['fechaNacimiento']; }
+        
+                    $resp = $this->modificarPaciente();
+                    if($resp){
+                        $respuesta = $_respuestas->response;
+                        $respuesta["result"] = array(
+                            "pacienteId" => $this->pacienteid
+                        );
+                        return $respuesta;
+                    }else{
+                        return $_respuestas->error_500();
+                    }
+                }
+
+            }else{
+                return $_respuestas->error_401("El Token que envio es invalido o ha caducado");
+            }
+        }
+
+
+    }
+
+
+    private function modificarPaciente(){
+        $query = "UPDATE " . $this->table . " SET Nombre ='" . $this->nombre . "',Direccion = '" . $this->direccion . "', DNI = '" . $this->dni . "', CodigoPostal = '" .
+        $this->codigoPostal . "', Telefono = '" . $this->telefono . "', Genero = '" . $this->genero . "', FechaNacimiento = '" . $this->fechaNacimiento . "', Correo = '" . $this->correo .
+         "' WHERE PacienteId = '" . $this->pacienteid . "'"; 
+        $resp = parent::nonQuery($query);
+        if($resp >= 1){
+             return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+    public function delete($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json,true);
+
+        if(!isset($datos['token'])){
+            return $_respuestas->error_401();
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken =   $this->buscarToken();
+            if($arrayToken){
+
+                if(!isset($datos['pacienteId'])){
+                    return $_respuestas->error_400();
+                }else{
+                    $this->pacienteid = $datos['pacienteId'];
+                    $resp = $this->eliminarPaciente();
+                    if($resp){
+                        $respuesta = $_respuestas->response;
+                        $respuesta["result"] = array(
+                            "pacienteId" => $this->pacienteid
+                        );
+                        return $respuesta;
+                    }else{
+                        return $_respuestas->error_500();
+                    }
+                }
+
+            }else{
+                return $_respuestas->error_401("El Token que envio es invalido o ha caducado");
+            }
+        }
+
+
+
+     
+    }
+
+
+    private function eliminarPaciente(){
+        $query = "DELETE FROM " . $this->table . " WHERE PacienteId= '" . $this->pacienteid . "'";
+        $resp = parent::nonQuery($query);
+        if($resp >= 1 ){
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+    private function buscarToken(){
+        $query = "SELECT  TokenId,UsuarioId,Estado from usuarios_token WHERE Token = '" . $this->token . "' AND Estado = 'Activo'";
+        $resp = parent::getDatos($query);
+        if($resp){
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+    private function actualizarToken($tokenid){
+        $date = date("Y-m-d H:i");
+        $query = "UPDATE usuarios_token SET Fecha = '$date' WHERE TokenId = '$tokenid' ";
+        $resp = parent::nonQuery($query);
+        if($resp >= 1){
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+
+}
+
+
+
+
+
+?>
---
+++ apirest_YT/sections.php
@@ -0,0 +1,121 @@
+<?php
+require_once 'clases/respuestas.class.php';
+require_once 'clases/sections.class.php';
+
+$_respuestas = new respuestas;
+$_secciones = new sections;
+
+
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+
+    //   * Esta solicitud devuelve el detalle del producto indicado segun su ID
+    //     http://url.com/apirest/products?id=1
+    if(isset($_GET['id'])){
+        $productoid = $_GET['id'];
+        $datosProducto = $_productos->getProduct($productoid);
+        header("Content-Type: application/json");
+        echo json_encode($datosProducto);
+        http_response_code(200);
+
+    // Aqui vamos a listar los productos en base a los PARAMETROS enviados desde la app
+    // Esta solicitud devuelve la pagina solicitada segun la categoria (categ) seleccionada
+    // con una cantidad de registros indicada en pageSize.
+    //   * si categ no es enviada su valor por defecto es 0 y se listarian todos los item existentes
+    //   * Si page no es enviada su valor por defecto en 1.
+    //   * Si pageSize no es enviada su valor por defecto es 10.
+    //   * si categ es enviada y requiere listar la totalidad de item especifique pageSize=all 
+
+    //    http://url.com/apirest/products?categ=1&page=1&pageSize=10
+
+    }else if(isset($_GET["id"])){
+        $categoria = $_GET["categ"];
+        $pagina = 1;
+        $cantidad = 10;
+
+        if(isset($_GET["page"])){ $pagina = $_GET["page"]; }
+        if(isset($_GET["pageSize"])){ $cantidad = $_GET["pageSize"]; }
+
+        //print_r("Categoria = ".$categoria." Pagina = ".$pagina." Cantidad = ".$cantidad." " );
+
+        $listaProductos = $_productos->getProductsList( $categoria, $pagina, $cantidad );
+        header("Content-Type: application/json");
+        echo json_encode($listaProductos);
+        http_response_code(200);
+
+        } else {
+            // revisar estas lineas ... para ver si son necesarias
+            // $categoria=0;
+            // $pagina=0;
+            // $cantidad=0;
+            // $listaProductos = $_productos->getProductsList( $categoria, $pagina, $cantidad );
+            // header("Content-Type: application/json");
+            // echo json_encode($listaProductos);
+            // http_response_code(200);
+        }
+    
+}else if($_SERVER['REQUEST_METHOD'] == "POST"){
+    //recibimos los datos enviados
+    $postBody = file_get_contents("php://input");
+    //enviamos los datos al manejador
+    $datosArray = $_pacientes->post($postBody);
+    //delvovemos una respuesta 
+     header('Content-Type: application/json');
+     if(isset($datosArray["detail"]["id"])){
+         $responseCode = $datosArray["detail"]["id"];
+         http_response_code($responseCode);
+     }else{
+         http_response_code(200);
+     }
+     echo json_encode($datosArray);
+    
+}else if($_SERVER['REQUEST_METHOD'] == "PUT"){
+      //recibimos los datos enviados
+      $postBody = file_get_contents("php://input");
+      //enviamos datos al manejador
+      $datosArray = $_pacientes->put($postBody);
+        //delvovemos una respuesta 
+     header('Content-Type: application/json');
+     if(isset($datosArray["result"]["error_id"])){
+         $responseCode = $datosArray["result"]["error_id"];
+         http_response_code($responseCode);
+     }else{
+         http_response_code(200);
+     }
+     echo json_encode($datosArray);
+
+}else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+
+        $headers = getallheaders();
+        if(isset($headers["token"]) && isset($headers["pacienteId"])){
+            //recibimos los datos enviados por el header
+            $send = [
+                "token" => $headers["token"],
+                "pacienteId" =>$headers["pacienteId"]
+            ];
+            $postBody = json_encode($send);
+        }else{
+            //recibimos los datos enviados
+            $postBody = file_get_contents("php://input");
+        }
+        
+        //enviamos datos al manejador
+        $datosArray = $_pacientes->delete($postBody);
+        //delvovemos una respuesta 
+        header('Content-Type: application/json');
+        if(isset($datosArray["result"]["error_id"])){
+            $responseCode = $datosArray["result"]["error_id"];
+            http_response_code($responseCode);
+        }else{
+            http_response_code(200);
+        }
+        echo json_encode($datosArray);
+       
+
+}else{
+    header('Content-Type: application/json');
+    $datosArray = $_respuestas->error_405();
+    echo json_encode($datosArray);
+}
+
+
+?>
